dc008fcb73b3749b65af0647d18cff675ba51cad
Paul Knapp <ppankluap@gmail.com>
2025-03-02 19:37:59 -0600

    Fix: rails helper line added

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,3 +1,5 @@
+require "rails_helper"
+
 RSpec.describe Card, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
@@ -20,7 +22,6 @@ RSpec.describe Card, type: :model do
       }
       user_info = {user: testuser, binder_id: testuser.binders[0].id}
       binder_card = Card.create_new_card(card_params, user_info)
-      binding.pry
       expect(binder_card).to be_instance_of(BinderCard)
     
     end
