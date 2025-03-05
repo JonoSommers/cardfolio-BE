a34f26dff614230ee2532c5505a4eb077dc0e0aa
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-03-04 17:05:45 -0700

    same as last commit

--- spec/requests/api/v1/binder_request_spec.rb
+++ spec/requests/api/v1/binder_request_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe "Binder Controller", type: :request do 
 
   before :each do 
@@ -34,11 +34,27 @@ RSpec.describe "Binder Controller", type: :request do
         "binder_name": "My Tournament Deck" 
       }
       
-    patch "http://localhost:3000/api/v1/users/#{@testuser.id}/binders/#{@testuser.binders[0].id}", params: binder_params, as: :json
-    json = JSON.parse(response.body, symbolize_names: true)[:data]
+      patch "http://localhost:3000/api/v1/users/#{@testuser.id}/binders/#{@testuser.binders[0].id}", params: binder_params, as: :json
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(json[:attributes][:name]).to be_a(String)
-    expect(json[:attributes][:name]).to eq("My Tournament Deck")
+      expect(json[:attributes][:name]).to be_a(String)
+      expect(json[:attributes][:name]).to eq("My Tournament Deck")
+    end
+  end
+
+  describe "Shows a 400 error" do 
+    it "Can show a status 400 error" do
+      binder_params = {
+        "binder_name": "My Tournament Deck" 
+      }
+
+      post "http://localhost:3000/api/v1/users/#{@testuser.id}/binders", params: binder_params, as: :json
+      post "http://localhost:3000/api/v1/users/#{@testuser.id}/binders", params: binder_params, as: :json
+  
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(json[:errors]).to be_present
     end
   end
 end
