5696ecb96bb3f7e4daafb6b9328d3d19aee5dbc9
Matt Haefling <m.haefling@protonmail.com>
2025-03-06 10:54:36 -0700

    Updated User Serializer so that index and create are no longer returning all user info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     user = User.all 
-    render json: UserSerializer.new(user)
+    render json: UserSerializer.new(user, { params: {} }), status: :ok
   end
 
   def create
     user = User.create!(username: params[:username])
-    render json: UserSerializer.new(user)
+    render json: UserSerializer.new(user, { params: {} }), status: :ok
   end
 
   def show
     user = User.find(params[:id])
-    render json: UserSerializer.new(user), status: :ok
+    render json: UserSerializer.new(user, { params: { show_binders: true } }), status: :ok
   end
 
   private
