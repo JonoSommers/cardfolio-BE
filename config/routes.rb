835e38eeb620eef0fcba03bf68ad6fb13cc45c33
Jono Sommers <jonosommers95@gmail.com>
2025-03-07 11:56:02 -0600

    removed commented code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   namespace :api do
@@ -14,6 +10,4 @@ Rails.application.routes.draw do
       end
     end
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
