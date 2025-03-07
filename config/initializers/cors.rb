c06eac68785e847c61fe35a90af6d3ec10e76a3c
Jono Sommers <jonosommers95@gmail.com>
2025-03-07 14:50:01 -0600

    coors now allows access for local and deployed urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://cardfolio-fe.onrender.com/"
+    origins "https://cardfolio-fe.onrender.com/", "http://localhost:5173"
 
     resource "*",
       headers: :any,
